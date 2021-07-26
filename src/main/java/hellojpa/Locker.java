b23779d61278bfebbebbb9116e87bdd46b0a6c3b
Charles Kang <kscscy@gmail.com>
2021-07-26 18:07:19 +0900

    OneToOne two-way

--- src/main/java/hellojpa/Locker.java
+++ src/main/java/hellojpa/Locker.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Locker {
@@ -12,5 +9,8 @@ public class Locker {
     private Long id;
 
     private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
 
 }
