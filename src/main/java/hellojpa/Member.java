31bd4678d47353dcdaa9b7fc11496dce9b7fdc2e
Charles Kang <kscscy@gmail.com>
2021-07-26 18:02:38 +0900

    onetoone one-way

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -21,6 +21,10 @@ public class Member {
             insertable = false, updatable = false)
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     public Long getId() {
         return id;
     }
