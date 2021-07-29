212238f2161f0497760f4515bd6561219b67ab10
Charles Kang <kscscy@gmail.com>
2021-07-29 16:12:06 +0900

    valueType collection

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,26 +20,57 @@ public class JpaMain {
 
         try {
             // 영속
-            Address address = new Address("city1", "street1", "zipcode1");
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
+            member.setHomeAddress(new Address("homeCity", "street1", "zipcode1"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("피자");
+            member.getFavoriteFoods().add("족발");
+
+//            member.getAddressHistory().add(new Address("oldCity1", "street1", "zipcode1"));
+//            member.getAddressHistory().add(new Address("oldCity2", "street1", "zipcode1"));
+
+            // AddressEntity
+            member.getAddressHistory().add(new AddressEntity("oldCity1", "street1", "zipcode1"));
+            member.getAddressHistory().add(new AddressEntity("oldCity2", "street1", "zipcode1"));
+
+
             em.persist(member);
 
-            /*
-//            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setHomeAddress(copyAddress);
-            em.persist(member2);
-//            member.getHomeAddress().setCity("newCity");
-*/
+            em.flush();
+            em.clear();
 
-            // address 를 통으로 갈아끼는 것이 맞다
-            // 또는 필요하면 내부에 copy 메소드를 만든다
-            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
-            member.setHomeAddress(newAddress);
+            System.out.println("============find==============");
+            Member findMember = em.find(Member.class, member.getId());
+
+            /*
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address = " + address.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
+             */
+
+            // homeCity -> newCity
+//            findMember.getHomeAddress().setCity("newCity") // side Effect 발생할 수 있다.
+
+//            Address a = findMember.getHomeAddress();
+//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            // 치킨 -> 한식
+//            findMember.getFavoriteFoods().remove("치킨"); // String 자체가 값 타입이다.
+//            findMember.getFavoriteFoods().add("한식");
+
+            // oldCity1 만 변경하고 싶다 => Address 인스턴스를 통으로 갈아끼워야한다.
+            // remove()로 제거하기 위해서는 equals(), hashCode() 구현돼있어야 한다.
+//            findMember.getAddressHistory().remove(new Address("oldCity1", "street1", "zipcode1"));
+//            findMember.getAddressHistory().add(new Address("newCity1", "street1", "zipcode1"));
 
             System.out.println("=============================");
 
