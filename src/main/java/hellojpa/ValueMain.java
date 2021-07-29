8215f7b9892c70c9f055d777ced5e7fc273e6ec3
Charles Kang <kscscy@gmail.com>
2021-07-29 14:52:06 +0900

    "valueType equals()"

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -4,20 +4,14 @@ public class ValueMain {
     public static void main(String[] args) {
 
         int a = 10;
-        // a 의 값이 복사가 돼서 b 로 넘어가기 때문에 따로 저장공간을 가진다
-        int b = a;
+        int b = 10;
 
-        b = 20;
+        System.out.println("a == b: " + (a == b));
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("city1", "street1", "zipcode1");
+        Address address2 = new Address("city1", "street1", "zipcode1");
 
-        Integer c = new Integer(10);
-        // 주소값(참조)이 넘어간다. 하지만 Integer 는 넘어가지 않음
-        Integer d = c;
-
-        System.out.println("c = " + c);
-        System.out.println("d = " + d);
-
+        System.out.println("address1 == address2: " + (address1 == address2));
+        System.out.println("address1.equals(address2): " + address1.equals(address2));
     }
 }
